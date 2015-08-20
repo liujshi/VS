c9d95d435a28ebfa7ad63094e25992a53cc026d1
liujshi <liujunshisysu@gmail.com>
2015-08-21 01:00:42 +0800

    smartpointer added

--- ConsoleApplication2/Test.cpp
+++ ConsoleApplication2/Test.cpp
@@ -3,15 +3,29 @@
 
 #include "stdafx.h"
 
+#include "SmartPointer.h"
 #include <iostream>
+#include <string>
 using namespace std;
+using namespace LIUSTL;
+
+class A{
+public:
+	A(){
+		cout << "A_Constructed!" << endl;
+	}
+	~A(){
+		cout << "A_Destructed!" << endl;
+	}
+};
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Hello_My_Git!" << endl;
-	//lalallalalala!
-	//wqd
+	A *a = new A;
+	SPtr<A>sp=new SPtr<A>;
+	sp = a;
+	auto sp1(a);
 
-	//wqdqdqw
 	return 0;
 
 }
